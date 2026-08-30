20b1494a2ba7a7be5f0c41c67f17313e1b286b60
yomilansuu <malansuu@gmail.com>
2026-08-30 23:09:29 +0800

    2026-08-30 23:09

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -43,10 +43,10 @@ export const siteConfig: SiteConfig = {
 	title: "文字型藥錠",
 
 	// 站点副标题
-	subtitle: "一直都在寫文",
+	subtitle: "施工中",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: " ",
 
 	// 站点描述
 	description:
